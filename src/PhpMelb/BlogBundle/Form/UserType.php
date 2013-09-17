8fb6c9bd8fab0e25f0bc9bb030612f9a9f246d68
Tom Corrigan <tommygnr@gmail.com>
2013-09-17 17:29:31 +1000

    Use custom User form with FOSUserBundle

---
+++ src/PhpMelb/BlogBundle/Form/UserType.php
@@ -0,0 +1,42 @@
+<?php
+
+namespace PhpMelb\BlogBundle\Form;
+
+use Symfony\Component\Form\AbstractType;
+use Symfony\Component\Form\FormBuilderInterface;
+use Symfony\Component\OptionsResolver\OptionsResolverInterface;
+use FOS\UserBundle\Form\Type\RegistrationFormType as BaseType;
+
+class UserType extends BaseType
+{
+        /**
+     * @param FormBuilderInterface $builder
+     * @param array $options
+     */
+    public function buildForm(FormBuilderInterface $builder, array $options)
+    {
+        parent::buildForm($builder, $options);
+
+        $builder
+            ->add('phone')
+        ;
+    }
+
+    /**
+     * @param OptionsResolverInterface $resolver
+     */
+    public function setDefaultOptions(OptionsResolverInterface $resolver)
+    {
+        $resolver->setDefaults(array(
+            'data_class' => 'PhpMelb\BlogBundle\Entity\User'
+        ));
+    }
+
+    /**
+     * @return string
+     */
+    public function getName()
+    {
+        return 'phpmelb_blogbundle_user';
+    }
+}
